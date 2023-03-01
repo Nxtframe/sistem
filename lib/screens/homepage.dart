c7a8838c98cda24caefbf1e89dec68e1f5df2454
IllegalTurtle <jkyyen@gmail.com>
2023-03-01 15:09:30 +0530

    Commit Cards

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
-      body: CardWidget(),
+      body: Column(children: [Row(),Row()]),
     );
   }
 }
